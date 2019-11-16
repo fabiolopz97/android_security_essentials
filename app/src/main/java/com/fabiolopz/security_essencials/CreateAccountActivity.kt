7efbfc1cbf0ee0f5cb182ff939d97a54ffac9ad2
Fabio <fabiolopz97@gmail.com>
2019-11-16 16:44:33 -0500

    Se agregaron las interfaces gráficas

---
+++ app/src/main/java/com/fabiolopz/security_essencials/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.fabiolopz.security_essencials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
